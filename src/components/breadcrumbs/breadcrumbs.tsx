d30272206880c47dff5049137fc3a439dddc2000
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2023-08-08 23:14:33 +0200

    fix

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -3,20 +3,32 @@ interface Props {
 }
 
 const Breadcrumbs: React.FC<Props> = ({ path }) => {
-  const pathDirs = path.split('/');
+  const pathDirs = path.split('/').filter(function (entry) {
+    return entry.trim() != '';
+  });
 
-  return (
-    <div className='md:hidden pt-6 px-6 last:bg-black font-bold text-xl'>
-      <a href={'/'}>gergus.no</a>
-      {pathDirs.map((dir) => {
-        if (dir != '')
+  const lastDir = pathDirs.pop();
+
+  if (lastDir)
+    return (
+      <div className='md:hidden pt-6 px-6 font-bold text-xl'>
+        <a href={'/'}>gergus.no</a>
+        {pathDirs.map((dir) => {
           return (
             <>
               <p className='inline text-accent'>/</p>
               <a href={dir}>{dir}</a>
             </>
           );
-      })}
+        })}
+        <p className='inline'>/</p>
+        <a className='border-b-[2px] border-accent'>{lastDir}</a>
+      </div>
+    );
+
+  return (
+    <div className='md:hidden pt-6 px-6 font-bold text-xl'>
+      <p className='border-b-[2px] border-accent w-min'>gergus.no</p>
     </div>
   );
 };
