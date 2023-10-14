ee8ca25d48c6ed8dd63b904b627cd96f7cc7cae0
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2023-10-14 21:05:17 +0200

    2000

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwindIntegration from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react(), tailwindIntegration()],
+  integrations: [tailwindIntegration()],
   experimental: { assets: true },
   markdown: {
     remarkPlugins: ['remark-math'],
